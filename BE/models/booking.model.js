69e62b1739b56c0320bd5d60ddf1b0f738ce61e8
nguyentiendat09 <luanoutplay@gmail.com>
2025-04-29 01:56:04 +0700

    Refactor: Remove voucher-related functionality
    - Deleted voucher.controller.js and voucher.model.js files to remove voucher management features.
    - Updated booking.model.js to remove references to vouchers.
    - Removed voucher routes from index.js and voucher.route.js.
    - Cleaned up cloudinary.js by removing uploadVoucher middleware.
    - Adjusted index.js to maintain code consistency and readability.

--- BE/models/booking.model.js
+++ BE/models/booking.model.js
@@ -1,87 +1,83 @@
 module.exports = (sequelize, Sequelize) => {
-    return sequelize.define(
-        "Booking",
-        {
-            id: {
-                type: Sequelize.INTEGER.UNSIGNED,
-                autoIncrement: true,
-                primaryKey: true,
-            },
-            discount_id: {
-                type: Sequelize.INTEGER,
-                allowNull: true,
-            },
-            user_id: {
-                type: Sequelize.INTEGER,
-                allowNull: true,
-            },
-            travel_tour_id: {
-                type: Sequelize.INTEGER,
-                allowNull: true,
-            },
-            booking_date: {
-                type: Sequelize.DATEONLY,
-                allowNull: true,
-            },
-            number_adult: {
-                type: Sequelize.INTEGER,
-                allowNull: true,
-                defaultValue: 0,
-            },
-            number_children: {
-                type: Sequelize.INTEGER,
-                allowNull: true,
-                defaultValue: 0,
-            },
-            number_toddler: {
-                type: Sequelize.INTEGER,
-                allowNull: true,
-                defaultValue: 0,
-            },
-            number_newborn: {
-                type: Sequelize.INTEGER,
-                allowNull: true,
-                defaultValue: 0,
-            },
-            total_cost: {
-                type: Sequelize.INTEGER,
-                allowNull: true,
-                defaultValue: 0,
-            },
-            status: {
-                type: Sequelize.INTEGER,
-                allowNull: true,
-                defaultValue: 0, // 0: Not Paid, 1: Half Paid, 2: Paid, 3: Canceled, 4: Refunded, 5: Completed, 6: Expired
-            },
-            name: {
-                type: Sequelize.STRING,
-                allowNull: true,
-            },
-            phone: {
-                type: Sequelize.STRING,
-            },
-            email: {
-                type: Sequelize.STRING,
-            },
-            address: {
-                type: Sequelize.STRING,
-            },
-            voucher_id: {
-                type: Sequelize.INTEGER,
-                allowNull: true,
-            },
-            note: {
-                type: Sequelize.TEXT,
-                allowNull: true,
-            },
-            booking_code: {
-                type: Sequelize.STRING,
-                allowNull: true,
-            },
-        },
-        {
-            tableName: "booking", // Đảm bảo tên bảng khớp với DB
-            timestamps: true, // Không dùng createdAt và updatedAt
-        }
-    );
+  return sequelize.define(
+    "Booking",
+    {
+      id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      discount_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      travel_tour_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      booking_date: {
+        type: Sequelize.DATEONLY,
+        allowNull: true,
+      },
+      number_adult: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 0,
+      },
+      number_children: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 0,
+      },
+      number_toddler: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 0,
+      },
+      number_newborn: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 0,
+      },
+      total_cost: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 0,
+      },
+      status: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 0, // 0: Not Paid, 1: Half Paid, 2: Paid, 3: Canceled, 4: Refunded, 5: Completed, 6: Expired
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      phone: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      address: {
+        type: Sequelize.STRING,
+      },
+      note: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      booking_code: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      tableName: "booking", // Đảm bảo tên bảng khớp với DB
+      timestamps: true, // Không dùng createdAt và updatedAt
+    }
+  );
 };
--- BE/models/index.js
+++ BE/models/index.js
@@ -1,19 +1,19 @@
-const {Sequelize} = require("sequelize");
+const { Sequelize } = require("sequelize");
 const config = require("../db/config.js");
 
 // Khởi tạo Sequelize với cấu hình từ `config.js`
 const sequelize = new Sequelize(config.database, config.user, config.password, {
-    host: config.host,
-    dialect: "mysql",
-    dialectModule: require("mysql2"),
-    logging: false, // Tắt log query SQL
+  host: config.host,
+  dialect: "mysql",
+  dialectModule: require("mysql2"),
+  logging: false, // Tắt log query SQL
 });
 
 // Kiểm tra kết nối
 sequelize
-    .authenticate()
-    .then(() => console.log("Connected to MySQL successfully"))
-    .catch((error) => console.error("Unable to connect to the database:", error));
+  .authenticate()
+  .then(() => console.log("Connected to MySQL successfully"))
+  .catch((error) => console.error("Unable to connect to the database:", error));
 
 // Import các model
 //Role
@@ -25,8 +25,8 @@ const Booking = require("./booking.model")(sequelize, Sequelize);
 //Restaurant
 const Restaurant = require("./restaurant.model.js")(sequelize, Sequelize);
 const RestaurantBooking = require("./restaurantBooking.model.js")(
-    sequelize,
-    Sequelize
+  sequelize,
+  Sequelize
 );
 //Hotel
 const Hotel = require("./hotel.model.js")(sequelize, Sequelize);
@@ -35,14 +35,14 @@ const HotelBooking = require("./hotelBooking.model.js")(sequelize, Sequelize);
 const TypeVehicle = require("./typeVehicle.model.js")(sequelize, Sequelize);
 const Vehicle = require("./vehicle.model.js")(sequelize, Sequelize);
 const VehicleBooking = require("./vehicleBooking.model.js")(
-    sequelize,
-    Sequelize
+  sequelize,
+  Sequelize
 );
 //Customer
 const Customer = require("./customer.model")(sequelize, Sequelize);
 const PostExperience = require("./postExperience.model.js")(
-    sequelize,
-    Sequelize
+  sequelize,
+  Sequelize
 );
 const Payment = require("./payment.model.js")(sequelize, Sequelize);
 
@@ -54,7 +54,6 @@ const Directory = require("./directory.model.js")(sequelize, Sequelize);
 //Tour
 const Tour = require("./tour.model")(sequelize, Sequelize);
 const Passenger = require("./passenger.model")(sequelize, Sequelize);
-const Voucher = require("./voucher.model")(sequelize, Sequelize);
 const TypeTour = require("./typeTour.model")(sequelize, Sequelize);
 const TravelTour = require("./travel_tour.model")(sequelize, Sequelize);
 const Feedback = require("./feedback.model")(sequelize, Sequelize);
@@ -64,19 +63,19 @@ const Location = require("./location.model.js")(sequelize, Sequelize);
 const Service = require("./service.model")(sequelize, Sequelize);
 const TourInfo = require("./tour_info.model.js")(sequelize, Sequelize);
 const DiscountService = require("./discountService.model.js")(
-    sequelize,
-    Sequelize
+  sequelize,
+  Sequelize
 );
 const ProgramDiscount = require("./programDiscount.model.js")(
-    sequelize,
-    Sequelize
+  sequelize,
+  Sequelize
 );
 const Topic = require("./topic.model")(sequelize, Sequelize);
 const TourActivities = require("./tour_activities.model")(sequelize, Sequelize);
 const TourService = require("./tour_service.model")(sequelize, Sequelize);
 const TravelGuideLocation = require("./travelGuideLocation.model.js")(
-    sequelize,
-    Sequelize
+  sequelize,
+  Sequelize
 );
 const FavoriteTour = require("./favoriteTour.model.js")(sequelize, Sequelize);
 const Like = require("./like.model.js")(sequelize, Sequelize);
@@ -85,35 +84,35 @@ const StaffLocation = require("./staffLocation.model.js")(sequelize, Sequelize);
 
 // Mối quan hệ (Associations)
 //Payment/Booking
-Booking.hasMany(Payment, {foreignKey: "booking_id"});
-Payment.belongsTo(Booking, {foreignKey: "booking_id"});
+Booking.hasMany(Payment, { foreignKey: "booking_id" });
+Payment.belongsTo(Booking, { foreignKey: "booking_id" });
 //Payment/Customer
 // Customer.hasMany(Payment, {foreignKey: "customer_id"});
 // Payment.belongsTo(Customer, {foreignKey: "customer_id"});
 
 // User/Booking
-User.hasMany(Booking, {foreignKey: "user_id"});
-Booking.belongsTo(User, {foreignKey: "user_id"});
+User.hasMany(Booking, { foreignKey: "user_id" });
+Booking.belongsTo(User, { foreignKey: "user_id" });
 
 //Booking/RestaurantBooking
-Booking.hasMany(RestaurantBooking, {foreignKey: "booking_id"});
-RestaurantBooking.belongsTo(Booking, {foreignKey: "booking_id"});
+Booking.hasMany(RestaurantBooking, { foreignKey: "booking_id" });
+RestaurantBooking.belongsTo(Booking, { foreignKey: "booking_id" });
 
 //RestaurantBooking/Booking
 // RestaurantBooking.hasMany(Booking, {foreignKey: "booking_id"});
 // Booking.belongsTo(RestaurantBooking, {foreignKey: "booking_id"});
 
 //Restaurant/RestaurantBooking
-Restaurant.hasMany(RestaurantBooking, {foreignKey: "restaurant_id"});
-RestaurantBooking.belongsTo(Restaurant, {foreignKey: "restaurant_id"});
+Restaurant.hasMany(RestaurantBooking, { foreignKey: "restaurant_id" });
+RestaurantBooking.belongsTo(Restaurant, { foreignKey: "restaurant_id" });
 
 //RestaurantBooking/Restaurant
 // RestaurantBooking.hasMany(Restaurant, {foreignKey: "restaurant_id"});
 // Restaurant.belongsTo(RestaurantBooking, {foreignKey: "restaurant_id"});
 
 //Location/Restaurant
-Location.hasMany(Restaurant, {foreignKey: "location_id"});
-Restaurant.belongsTo(Location, {foreignKey: "location_id"});
+Location.hasMany(Restaurant, { foreignKey: "location_id" });
+Restaurant.belongsTo(Location, { foreignKey: "location_id" });
 
 //Booking/HotelBooking
 // Booking.hasMany(HotelBooking, {foreignKey: "booking_id"});
@@ -124,37 +123,36 @@ Restaurant.belongsTo(Location, {foreignKey: "location_id"});
 // Booking.belongsTo(HotelBooking, {foreignKey: "booking_id"});
 
 //Location/Hotel
-Location.hasMany(Hotel, {foreignKey: "location_id"});
-Hotel.belongsTo(Location, {foreignKey: "location_id"});
+Location.hasMany(Hotel, { foreignKey: "location_id" });
+Hotel.belongsTo(Location, { foreignKey: "location_id" });
 
 //Booking/HotelBooking
-Booking.hasMany(HotelBooking, {foreignKey: "booking_id"});
-HotelBooking.belongsTo(Booking, {foreignKey: "booking_id"});
+Booking.hasMany(HotelBooking, { foreignKey: "booking_id" });
+HotelBooking.belongsTo(Booking, { foreignKey: "booking_id" });
 
 // //Hotel/HotelBooking
-Hotel.hasMany(HotelBooking, {foreignKey: "hotel_id"});
-HotelBooking.belongsTo(Hotel, {foreignKey: "hotel_id"});
+Hotel.hasMany(HotelBooking, { foreignKey: "hotel_id" });
+HotelBooking.belongsTo(Hotel, { foreignKey: "hotel_id" });
 
 //HotelBooking/Hotel
 // HotelBooking.hasMany(Hotel, {foreignKey: "hotel_id"});
 // Hotel.belongsTo(HotelBooking, {foreignKey: "hotel_id"});
 
 //Booking/VehicleBooking
-Booking.hasMany(VehicleBooking, {foreignKey: "booking_id"});
-VehicleBooking.belongsTo(Booking, {foreignKey: "booking_id"});
+Booking.hasMany(VehicleBooking, { foreignKey: "booking_id" });
+VehicleBooking.belongsTo(Booking, { foreignKey: "booking_id" });
 
 //Vehicle/VehicleBooking
-Vehicle.hasMany(VehicleBooking, {foreignKey: "vehicle_id"});
-VehicleBooking.belongsTo(Vehicle, {foreignKey: "vehicle_id"});
+Vehicle.hasMany(VehicleBooking, { foreignKey: "vehicle_id" });
+VehicleBooking.belongsTo(Vehicle, { foreignKey: "vehicle_id" });
 
 //User/Customer
-User.hasOne(Customer, {foreignKey: "user_id"});
-Customer.belongsTo(User, {foreignKey: "user_id"});
-
+User.hasOne(Customer, { foreignKey: "user_id" });
+Customer.belongsTo(User, { foreignKey: "user_id" });
 
 //Feedback/User
-User.hasMany(Feedback, {foreignKey: "user_id"});
-Feedback.belongsTo(User, {foreignKey: "user_id", as: "user"});
+User.hasMany(Feedback, { foreignKey: "user_id" });
+Feedback.belongsTo(User, { foreignKey: "user_id", as: "user" });
 
 //Hotel/Location
 // Location.hasMany(Hotel, {foreignKey: "location_id"});
@@ -164,216 +162,213 @@ Feedback.belongsTo(User, {foreignKey: "user_id", as: "user"});
 // Location.hasMany(Hotel, {foreignKey: "location_id"});
 // Hotel.belongsTo(Location, {foreignKey: "location_id"});
 
-
-
 //Feedback/TravelGuide
-TravelGuide.hasMany(Feedback, {foreignKey: "travel_guide_id"});
+TravelGuide.hasMany(Feedback, { foreignKey: "travel_guide_id" });
 Feedback.belongsTo(TravelGuide, {
-    foreignKey: "travel_guide_id",
-    as: "travelGuide",
+  foreignKey: "travel_guide_id",
+  as: "travelGuide",
 });
 
 //Tour/Feedback
-Tour.hasMany(Feedback, {foreignKey: "tour_id"});
-Feedback.belongsTo(Tour, {foreignKey: "tour_id", as: "tour"});
+Tour.hasMany(Feedback, { foreignKey: "tour_id" });
+Feedback.belongsTo(Tour, { foreignKey: "tour_id", as: "tour" });
 
 //Tour/Topic
-Tour.belongsTo(Topic, {foreignKey: "topic_id", as: "topic"});
-Topic.hasMany(Tour, {foreignKey: "topic_id"});
+Tour.belongsTo(Topic, { foreignKey: "topic_id", as: "topic" });
+Topic.hasMany(Tour, { foreignKey: "topic_id" });
 
 //Tour/TourService
 Tour.belongsToMany(Service, {
-    through: TourService,
-    foreignKey: "tour_id",
-    otherKey: "service_id",
-    as: "Services",
+  through: TourService,
+  foreignKey: "tour_id",
+  otherKey: "service_id",
+  as: "Services",
 });
 Service.belongsToMany(Tour, {
-    through: TourService,
-    foreignKey: "service_id",
-    otherKey: "tour_id",
-    as: "Tours",
+  through: TourService,
+  foreignKey: "service_id",
+  otherKey: "tour_id",
+  as: "Tours",
 });
 
 //Tour/TravelTour
 //Tour/DepartureSchedule
-Tour.hasMany(TravelTour, {foreignKey: "tour_id"});
-TravelTour.belongsTo(Tour, {foreignKey: "tour_id"});
+Tour.hasMany(TravelTour, { foreignKey: "tour_id" });
+TravelTour.belongsTo(Tour, { foreignKey: "tour_id" });
 
 //Location/Tour
-Location.hasMany(Tour, {foreignKey: "start_location"});
-Location.hasMany(Tour, {foreignKey: "end_location"});
-Tour.belongsTo(Location, {foreignKey: "start_location", as: "startLocation"});
-Tour.belongsTo(Location, {foreignKey: "end_location", as: "endLocation"});
+Location.hasMany(Tour, { foreignKey: "start_location" });
+Location.hasMany(Tour, { foreignKey: "end_location" });
+Tour.belongsTo(Location, { foreignKey: "start_location", as: "startLocation" });
+Tour.belongsTo(Location, { foreignKey: "end_location", as: "endLocation" });
 
 //ProgramDiscount/DiscountService
-ProgramDiscount.hasMany(DiscountService, {foreignKey: "program_discount_id"});
+ProgramDiscount.hasMany(DiscountService, { foreignKey: "program_discount_id" });
 DiscountService.belongsTo(ProgramDiscount, {
-    foreignKey: "program_discount_id",
-    as: "programDiscount",
+  foreignKey: "program_discount_id",
+  as: "programDiscount",
 });
 
 //DepartureSchedule/DiscountService
-TravelTour.hasMany(DiscountService, {foreignKey: "travel_tour_id"});
+TravelTour.hasMany(DiscountService, { foreignKey: "travel_tour_id" });
 DiscountService.belongsTo(TravelTour, {
-    foreignKey: "travel_tour_id",
-    as: "travelTour",
+  foreignKey: "travel_tour_id",
+  as: "travelTour",
 });
 
 //TravelGuide/GuideTour
-TravelGuide.hasMany(GuideTour, {foreignKey: "travel_guide_id"});
+TravelGuide.hasMany(GuideTour, { foreignKey: "travel_guide_id" });
 GuideTour.belongsTo(TravelGuide, {
-    foreignKey: "travel_guide_id",
-    as: "travelGuide",
+  foreignKey: "travel_guide_id",
+  as: "travelGuide",
 });
 
 //DepartureSchedule/GuideTour
-TravelTour.hasMany(GuideTour, {foreignKey: "travel_tour_id"});
+TravelTour.hasMany(GuideTour, { foreignKey: "travel_tour_id" });
 GuideTour.belongsTo(TravelTour, {
-    foreignKey: "travel_tour_id",
-    as: "travelTour",
+  foreignKey: "travel_tour_id",
+  as: "travelTour",
 });
 
 //User/TravelGuide
-User.hasOne(TravelGuide, {foreignKey: "user_id"});
-TravelGuide.belongsTo(User, {foreignKey: "user_id", as: "user"});
+User.hasOne(TravelGuide, { foreignKey: "user_id" });
+TravelGuide.belongsTo(User, { foreignKey: "user_id", as: "user" });
 
 //User(user_id)/TravelGuide(staff_id)
-User.hasMany(TravelGuide, {foreignKey: "staff_id"});
-TravelGuide.belongsTo(User, {foreignKey: "staff_id", as: "staff"});
+User.hasMany(TravelGuide, { foreignKey: "staff_id" });
+TravelGuide.belongsTo(User, { foreignKey: "staff_id", as: "staff" });
 
 //Role/User
-Role.hasMany(User, {foreignKey: "role_id"});
-User.belongsTo(Role, {foreignKey: "role_id", as: "role"});
+Role.hasMany(User, { foreignKey: "role_id" });
+User.belongsTo(Role, { foreignKey: "role_id", as: "role" });
 
 //PostExperience/User
-User.hasMany(PostExperience, {foreignKey: "user_id"});
-PostExperience.belongsTo(User, {foreignKey: "user_id", as: "user"});
-
+User.hasMany(PostExperience, { foreignKey: "user_id" });
+PostExperience.belongsTo(User, { foreignKey: "user_id", as: "user" });
 
 //DepartureSchedule/Booking
-TravelTour.hasMany(Booking, {foreignKey: "travel_tour_id"});
-Booking.belongsTo(TravelTour, {foreignKey: "travel_tour_id"});
+TravelTour.hasMany(Booking, { foreignKey: "travel_tour_id" });
+Booking.belongsTo(TravelTour, { foreignKey: "travel_tour_id" });
 
 //TypeTour/Tour
-TypeTour.hasMany(Tour, {foreignKey: "type_id"});
-Tour.belongsTo(TypeTour, {foreignKey: "type_id", as: "typeTour"});
+TypeTour.hasMany(Tour, { foreignKey: "type_id" });
+Tour.belongsTo(TypeTour, { foreignKey: "type_id", as: "typeTour" });
 
 //Passenger/Booking
-Passenger.belongsTo(Booking, {foreignKey: "booking_id", as: "booking"});
-Booking.hasMany(Passenger, {foreignKey: "booking_id", as: "passengers"});
+Passenger.belongsTo(Booking, { foreignKey: "booking_id", as: "booking" });
+Booking.hasMany(Passenger, { foreignKey: "booking_id", as: "passengers" });
 
 //Tour/TourActivities
-Tour.hasMany(TourActivities, {foreignKey: "tour_id", as: "tourActivities"});
-TourActivities.belongsTo(Tour, {foreignKey: "tour_id", as: "tour"});
+Tour.hasMany(TourActivities, { foreignKey: "tour_id", as: "tourActivities" });
+TourActivities.belongsTo(Tour, { foreignKey: "tour_id", as: "tour" });
 
 //User/Aritcle
-User.hasMany(Article, {foreignKey: "user_id"});
-Article.belongsTo(User, {foreignKey: "user_id", as: "author"});
+User.hasMany(Article, { foreignKey: "user_id" });
+Article.belongsTo(User, { foreignKey: "user_id", as: "author" });
 
 //Directory/Article
-Directory.hasMany(Article, {foreignKey: "directory_id"});
-Article.belongsTo(Directory, {foreignKey: "directory_id", as: "directory"});
+Directory.hasMany(Article, { foreignKey: "directory_id" });
+Article.belongsTo(Directory, { foreignKey: "directory_id", as: "directory" });
 
 //TravelGuide/TravelGuideLocation
 TravelGuide.hasMany(TravelGuideLocation, {
-    foreignKey: "travel_guide_id",
+  foreignKey: "travel_guide_id",
 });
 TravelGuideLocation.belongsTo(TravelGuide, {
-    foreignKey: "travel_guide_id",
-    as: "TravelGuideLocations",
+  foreignKey: "travel_guide_id",
+  as: "TravelGuideLocations",
 });
 
 //Location/TravelGuideLocation
-Location.hasMany(TravelGuideLocation, {foreignKey: "location_id"});
+Location.hasMany(TravelGuideLocation, { foreignKey: "location_id" });
 TravelGuideLocation.belongsTo(Location, {
-    foreignKey: "location_id",
-    as: "location",
+  foreignKey: "location_id",
+  as: "location",
 });
 
 //TravelGuide/Location
 TravelGuide.belongsToMany(Location, {
-    through: TravelGuideLocation,
-    foreignKey: "travel_guide_id",
-    otherKey: "location_id",
-    as: "locations",
+  through: TravelGuideLocation,
+  foreignKey: "travel_guide_id",
+  otherKey: "location_id",
+  as: "locations",
 });
 
 //FavoriteTour/User
-User.hasMany(FavoriteTour, {foreignKey: "user_id"});
-FavoriteTour.belongsTo(User, {foreignKey: "user_id", as: "user"});
+User.hasMany(FavoriteTour, { foreignKey: "user_id" });
+FavoriteTour.belongsTo(User, { foreignKey: "user_id", as: "user" });
 
 //FavoriteTour/Tour
-Tour.hasMany(FavoriteTour, {foreignKey: "tour_id"});
-FavoriteTour.belongsTo(Tour, {foreignKey: "tour_id", as: "tour"});
+Tour.hasMany(FavoriteTour, { foreignKey: "tour_id" });
+FavoriteTour.belongsTo(Tour, { foreignKey: "tour_id", as: "tour" });
 
 //Like/User
-User.hasMany(Like, {foreignKey: "user_id"});
-Like.belongsTo(User, {foreignKey: "user_id", as: "user"});
+User.hasMany(Like, { foreignKey: "user_id" });
+Like.belongsTo(User, { foreignKey: "user_id", as: "user" });
 
 //Like/Feedback
 Feedback.hasMany(Like, {
-    foreignKey: "target_id",
-    constraints: false,
-    scope: {target_type: "feedback"},
+  foreignKey: "target_id",
+  constraints: false,
+  scope: { target_type: "feedback" },
 });
 Like.belongsTo(Feedback, {
-    foreignKey: "target_id",
-    constraints: false,
-    as: "feedback",
+  foreignKey: "target_id",
+  constraints: false,
+  as: "feedback",
 });
 
 // Like/PostExperience
 PostExperience.hasMany(Like, {
-    foreignKey: "target_id",
-    constraints: false,
-    scope: {target_type: "postExperience"},
+  foreignKey: "target_id",
+  constraints: false,
+  scope: { target_type: "postExperience" },
 });
 Like.belongsTo(PostExperience, {
-    foreignKey: "target_id",
-    constraints: false,
-    as: "postExperience",
+  foreignKey: "target_id",
+  constraints: false,
+  as: "postExperience",
 });
 
 // Like/Article
 Article.hasMany(Like, {
-    foreignKey: "target_id",
-    constraints: false,
-    scope: {target_type: "article"},
+  foreignKey: "target_id",
+  constraints: false,
+  scope: { target_type: "article" },
 });
 Like.belongsTo(Article, {
-    foreignKey: "target_id",
-    constraints: false,
-    as: "article",
+  foreignKey: "target_id",
+  constraints: false,
+  as: "article",
 });
 
 //Passenger/TravelGuide
 Passenger.belongsTo(TravelGuide, {
-    foreignKey: "travel_guide_id",
-    as: "travelGuide",
+  foreignKey: "travel_guide_id",
+  as: "travelGuide",
 });
 TravelGuide.hasMany(Passenger, {
-    foreignKey: "travel_guide_id",
-    as: "passengers",
+  foreignKey: "travel_guide_id",
+  as: "passengers",
 });
 
 //Tour/TourInfo
-Tour.hasMany(TourInfo, {foreignKey: "tour_id", as: "tourInfo"});
-TourInfo.belongsTo(Tour, {foreignKey: "tour_id", as: "tour"});
+Tour.hasMany(TourInfo, { foreignKey: "tour_id", as: "tourInfo" });
+TourInfo.belongsTo(Tour, { foreignKey: "tour_id", as: "tour" });
 
 //User/StaffProfile
-User.hasOne(StaffProfile, {foreignKey: "user_id"});
-StaffProfile.belongsTo(User, {foreignKey: "user_id", as: "user"});
+User.hasOne(StaffProfile, { foreignKey: "user_id" });
+StaffProfile.belongsTo(User, { foreignKey: "user_id", as: "user" });
 
 //User/StaffLocation
-User.hasMany(StaffLocation, {foreignKey: "user_id"});
-StaffLocation.belongsTo(User, {foreignKey: "user_id", as: "user"});
+User.hasMany(StaffLocation, { foreignKey: "user_id" });
+StaffLocation.belongsTo(User, { foreignKey: "user_id", as: "user" });
 
 //Location/StaffLocation
-Location.hasMany(StaffLocation, {foreignKey: "location_id"});
+Location.hasMany(StaffLocation, { foreignKey: "location_id" });
 StaffLocation.belongsTo(Location, {
-    foreignKey: "location_id",
-    as: "location",
+  foreignKey: "location_id",
+  as: "location",
 });
 
 // Đối tượng `db` để chứa Sequelize và Models
@@ -398,13 +393,11 @@ db.Customer = Customer;
 db.PostExperience = PostExperience;
 db.Payment = Payment;
 
-
 db.Role = Role;
 
 db.Tour = Tour;
 db.TypeTour = TypeTour;
 db.Passenger = Passenger;
-db.Voucher = Voucher;
 db.TravelTour = TravelTour;
 db.Feedback = Feedback;
 db.GuideTour = GuideTour;
