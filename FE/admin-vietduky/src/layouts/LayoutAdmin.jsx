74df3de0bc3c83b71d63d3b5300559af6849dcc8
thetoannn <naotehtgnoud@gmail.com>
2025-05-02 15:53:33 +0700

    fix Sidebar

--- FE/admin-vietduky/src/layouts/LayoutAdmin.jsx
+++ FE/admin-vietduky/src/layouts/LayoutAdmin.jsx
@@ -12,7 +12,7 @@ export default function LayoutAdmin() {
     };
 
     return (
-        <div className="flex flex-col md:flex-row h-screen">
+        <div className="flex flex-col md:flex-row min-h-screen">
             <SidebarAdmin setBreadcrumb={setBreadcrumb} isCollapsed={isCollapsed} />
             <div className="flex-1">
                 <HeaderManage
